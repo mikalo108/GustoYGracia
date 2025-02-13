ca9f8bd878d0365aba16de32a19b70603fbc73c2
mikalo108 <miguelcarnicerembid@gmail.com>
2025-02-13 19:27:25 +0100

    migration add

---
+++ database/migrations/2025_02_13_175134_create_recipe_details_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipe_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('recipe_id')->unique();
+            $table->integer('prep_time');
+            $table->string('difficulty_level', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipe_details');
+    }
+};
---
+++ database/migrations/2025_02_13_175155_create_contact_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('surname', 100);
+            $table->string('bio', 2000);
+            $table->string('phone', 20);
+            $table->string('country', 100);
+            $table->string('city', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact');
+    }
+};
