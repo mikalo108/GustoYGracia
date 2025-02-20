bfc952c376088e51ee8fbab2e0032b295d61ca0a
mikalo108 <miguelcarnicerembid@gmail.com>
2025-02-20 19:21:21 +0100

    empezamos con controladores!

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function index() { 
+        $categoryList = Category::all();
+        return view('categories/all', ['categoryList'=>$categoryList]);
+    }
+
+    public function create() { 
+        $categories = Category::all();
+        return view('categories/form', ['categories' => $categories]);  
+    }
+
+    public function store(Request $r) { 
+        
+    }
+
+    public function edit($id) { 
+    }
+
+    public function update($id, Request $r) { 
+        
+    }
+    public function destroy($id) { 
+        
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('category', 'CategoryController');
+Route::get('/categories/delete/{category_id}', 'CategoryController@destroy')->name('category.myDestroy');
