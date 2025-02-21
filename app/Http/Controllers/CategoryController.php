cfe00ed10f4b16ee1e1409cf92db6c5655b9bd7d
mikalo108 <miguelcarnicerembid@gmail.com>
2025-02-21 17:04:25 +0100

    primeros controladores

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,25 +9,37 @@ class CategoryController extends Controller
 {
     public function index() { 
         $categoryList = Category::all();
-        return view('categories/all', ['categoryList'=>$categoryList]);
+        return view('category/all', ['categoryList'=>$categoryList]);
     }
 
     public function create() { 
         $categories = Category::all();
-        return view('categories/form', ['categories' => $categories]);  
+        return view('category/form', ['categories' => $categories]);  
     }
 
     public function store(Request $r) { 
-        
+        $c = new Category();
+        $c->name = $r->name;
+        $c->description = $r->description;
+        $c->save();
+        return redirect()->route('category.index');
     }
 
     public function edit($id) { 
+        $c = Category::find($id);
+        return view('category/form', ['category' => $c]);
     }
 
     public function update($id, Request $r) { 
-        
+        $c = Category::find($id);
+        $c->name = $r->name;
+        $c->description = $r->description;
+        $c->save();
+        return redirect()->route('category.index');
     }
     public function destroy($id) { 
-        
+        $c = Category::find($id);
+        $c->delete();
+        return redirect()->route('category.index');
     }
 }
--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    //
+    public function index() { 
+        $commentsList = Comment::all();
+        return view('comment/all', ['commentsList'=>$commentsList]);
+    }
+
+    public function create() { 
+        $comments = Comment::all();
+        return view('comment/form', ['comments' => $comments]);  
+    }
+
+    public function store(Request $r) { 
+        $c = new Comment();
+        $c->user_id = $r->user_id;
+        $c->recipe_id = $r->recipe_id;
+        $c->content = $r->content;
+        $c->save();
+        return redirect()->route('comment.index');
+    }
+
+    public function edit($id) { 
+        $c = Comment::find($id);
+        return view('comment/form', ['comment' => $c]);
+    }
+
+    public function update($id, Request $r) { 
+        $c = Comment::find($id);
+        $c->user_id = $r->user_id;
+        $c->recipe_id = $r->recipe_id;
+        $c->content = $r->content;
+        $c->save();
+        return redirect()->route('comment.index');
+    }
+    public function destroy($id) { 
+        $c = Comment::find($id);
+        $c->delete();
+        return redirect()->route('category.index');
+    }
 }
