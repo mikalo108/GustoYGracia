2960c86e341788380c9a23b61c5cc08679cae648
mikalo108 <miguelcarnicerembid@gmail.com>
2025-02-27 17:51:23 +0100

    images path

--- database/seeders/RecipeTableSeeder.php
+++ database/seeders/RecipeTableSeeder.php
@@ -23,12 +23,51 @@ class RecipeTableSeeder extends Seeder
         }
         
         DB::table('recipes')->insert([
-            'name' => 'Spaghetti Carbonara',
-            'image' => '',
-            'description' => 'A classic Italian pasta dish.',
-            'instructions' => 'Cook pasta, fry pancetta, mix with eggs and cheese.',
-            'user_id' => $users->random()->id
+            [
+                'name' => 'Spaghetti Carbonara',
+                'image' => 'spaghetti_carbonara.jpg',
+                'description' => 'A classic Italian pasta dish.',
+                'instructions' => 'Cook pasta, fry pancetta, mix with eggs and cheese.',
+                'user_id' => $users->random()->id,
+            ],
+            [
+                'name' => 'Pollo al Curry con Arroz Basmati',
+                'image' => 'pollo_curry.jpg',
+                'description' => 'Un plato aromático y especiado de la India.',
+                'instructions' => 'Saltear pollo con cebolla, ajo y especias de curry. Cocinar a fuego lento con leche de coco y servir con arroz basmati.',
+                'user_id' => $users->random()->id,
+            ],
+            [
+                'name' => 'Ensalada Caprese',
+                'image' => 'ensalada_caprese.jpg',
+                'description' => 'Una ensalada italiana sencilla y refrescante.',
+                'instructions' => 'Intercalar rodajas de tomate y mozzarella fresca. Rociar con aceite de oliva y hojas de albahaca.',
+                'user_id' => $users->random()->id,
+            ],
+            [
+                'name' => 'Salmón al Horno con Espárragos',
+                'image' => 'salmon_esparragos.jpg',
+                'description' => 'Una comida saludable y deliciosa.',
+                'instructions' => 'Sazonar filetes de salmón con sal, pimienta y limón. Hornear junto con espárragos hasta que estén tiernos.',
+                'user_id' => $users->random()->id,
+            ],
+            [
+                'name' => 'Lasaña de Carne',
+                'image' => 'lasaña_carne.jpg',
+                'description' => 'Un clásico plato italiano de pasta al horno.',
+                'instructions' => 'Alternar capas de pasta, salsa de carne, queso ricotta y mozzarella. Hornear hasta que esté dorado y burbujeante.',
+                'user_id' => $users->random()->id,
+            ],
+            [
+                'name' => 'Guacamole con Totopos',
+                'image' => 'guacamole_totopos.jpg',
+                'description' => 'Un aperitivo mexicano popular.',
+                'instructions' => 'Machacar aguacate con cebolla, tomate, cilantro, jalapeño y lima. Servir con totopos de maíz.',
+                'user_id' => $users->random()->id,
+            ],
         ]);
+
+        
 
     }
 }
