62cfff5c654078bb72d65b59958c7b5ee8d57ed7
Ines Baz <ins_baz94@hotmail.com>
2025-02-27 14:44:43 +0100

    view and css creation

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@push('css')
+    <!-- estilos adicionales -->
+    <style>
+
+    </style>
+@endpush
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>Bienvenido a nuestro Recetario</h1>
+    <p>Aquí encontrarás las mejores recetas para todos los gustos.</p>
+    <!-- Puedes agregar más contenido aquí -->
+@endsection
