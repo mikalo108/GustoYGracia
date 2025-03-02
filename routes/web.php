5dc2f77cf63d8c496c1465276ec3c7bbec4e7e6a
mikalo108 <miguelcarnicerembid@gmail.com>
2025-03-02 18:39:12 +0100

    post+

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::get('/myprofile', function () {
 })->name('myprofile');
 
 Route::resource('category', 'CategoryController');
+Route::post('/category/store/{category}', 'CategoryController@store')->name('category.store');
 Route::get('/category/show/{category}', 'CategoryController@show');
 Route::get('/category/delete/{category}', 'CategoryController@destroy')->name('category.myDestroy');
 
